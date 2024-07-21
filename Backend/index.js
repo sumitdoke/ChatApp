3f8407ae830dea320130db5d560bff676ea3c6d4
sumitdoke <96287752+sumitdoke@users.noreply.github.com>
2024-07-21 22:16:46 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
-// import cors from "cors";
+import cors from "cors";
 import { app, server } from "./socket/socket.js";
 dotenv.config({});
 
@@ -16,11 +16,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-//  const corsOption = {
-//     origin: 'chatapp-ashy-sigma.vercel.app',
-//     credentials: true
-// };
-// app.use(cors(corsOption));
+  const corsOption = {
+     origin: 'chatapp-ashy-sigma.vercel.app',
+     credentials: true
+ };
+ app.use(cors(corsOption));
 
 
 // routes
