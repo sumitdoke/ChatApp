671ef8bf9c7e9d6da0b86214041d07d2cef40942
sumitdoke <96287752+sumitdoke@users.noreply.github.com>
2024-07-21 22:53:18 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
   const corsOption = {
-     origin: 'https://chatapp-ashy-sigma.vercel.app',
+     origin: 'http://localhost:3000',
      credentials: true
  };
  app.use(cors(corsOption));
